466ca84fc1df66c4249cbdbf784409442e384c6d
Kest <meowtss@protonmail.com>
2025-01-14 21:34:13 -0500

    Redid project

--- Aeolian/Controllers/HomeController.cs
+++ Aeolian/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 using System.Diagnostics;
 using Aeolian.Models;
-using Aeolian.Services;
-using Isopoh.Cryptography.Argon2;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aeolian.Controllers
@@ -10,53 +7,20 @@ namespace Aeolian.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly UserService _userService;
 
-        public HomeController(ILogger<HomeController> logger, UserService userService)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _userService = userService;
         }
 
-        [Route("home")]
-        [Authorize]
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost("login")]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public IActionResult Login(User model)
+        public IActionResult Privacy()
         {
-            try
-            {
-                Console.WriteLine($"Attempting to authenticate user: {model.username}");
-
-                User userCheck = _userService.GetUser(UserService.USER_TYPE.GET, UserService.GET_TYPE.USERNAME, model.username);
-
-                if (userCheck.username == null)
-                {
-                    Console.WriteLine($"No use under the username {model.username}.");
-                    return View(nameof(Login));
-                }
-
-                if (!Argon2.Verify(userCheck.password, model.password))
-                {
-                    Console.WriteLine($"Failed to authenticate user: {model.username}");
-                    return View(nameof(Login));
-                }
-
-                Console.WriteLine($"Successfully authenticated user: {model.username}");
-                return View("home");
-            }
-            catch (Exception ex)
-            {
-                //Maybe replace this with a logger.
-                Console.WriteLine(ex.Message);
-                return View(nameof(Login));
-            }
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Aeolian/Program.cs
+++ Aeolian/Program.cs
@@ -1,18 +1,28 @@
-using Aeolian.Services;
-using Microsoft.AspNetCore.Hosting;
+using Aeolian.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton(typeof(UserService));
-builder.Services.Add(new ServiceDescriptor(typeof(UserService), UserService.GetInstance(builder.Configuration.GetConnectionString("AeolianConnection"))));
-
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -26,17 +36,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Login}/{id?}");
-
-    endpoints.MapControllerRoute(
-        name: "homePage",
-        pattern: "home",
-        defaults: new { controller = "Home", action = "Index" });
-});
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
